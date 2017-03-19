ae0d0945f0d1e39566c2dba536ca49c5b3d2b74d
Brydon Cheyney <brydon@remission.org.uk>
2017-03-19 01:38:08 +0000

    [provisioner] keybindings role for custom key mapping

---
+++ ansible/library/gsetting.py
@@ -0,0 +1,70 @@
+#!/usr/bin/python
+
+import json
+import re
+import subprocess
+
+from ansible.module_utils.basic import AnsibleModule
+
+def _escape_single_quotes(string):
+    return re.sub("'", r"'\''", string)
+
+def _set_value(schema, key, value):
+
+    command = " ".join([
+        'export `/usr/bin/dbus-launch`',
+        ';',
+        '/usr/bin/gsettings set', schema, key,
+        "'%s'" % _escape_single_quotes(value),
+        ';',
+        'kill $DBUS_SESSION_BUS_PID &> /dev/null'
+    ])
+
+    return subprocess.check_output(command, shell=True).strip()
+
+def _get_value(schema, key):
+
+    command = " ".join([
+        'export `/usr/bin/dbus-launch`',
+        ';',
+        '/usr/bin/gsettings get', schema, key,
+        ';',
+        'kill $DBUS_SESSION_BUS_PID &> /dev/null'
+    ])
+
+    return subprocess.check_output(command, shell=True).strip()
+
+def main():
+
+    module = AnsibleModule(
+        argument_spec = dict(
+            state     = dict(default='present', choices=['present', 'absent']),
+            schema    = dict(required=True),
+            key       = dict(required=True),
+            value     = dict(required=True)
+        ),
+        supports_check_mode = True
+    )
+
+    params = module.params
+    state = module.params['state']
+    schema = module.params['schema']
+    key = module.params['key']
+    value = module.params['value']
+
+    old_value = _get_value(schema, key)
+    changed = old_value != value
+
+    if changed and not module.check_mode:
+        _set_value(schema, key, value)
+
+    print json.dumps({
+        'changed': changed,
+        'schema': schema,
+        'key': key,
+        'value': value,
+        'old_value': old_value,
+    })
+
+if __name__ == '__main__':
+    main()
